c8faec67cd35bae58bb50b398023665bf03bae1e
Vijaydwivedi10 <84119245+Vijaydwivedi10@users.noreply.github.com>
2023-07-02 18:48:07 +0530

    Update files with project content

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Sidebar from './components/Sidebar';
 import Experience from './components/Experience';
-import Miscellaneous from './components/Miscellaneous';
 
 const App = () => {
   return (
