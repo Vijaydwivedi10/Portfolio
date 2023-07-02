f569eb29baa8a8642cb5f149dd6a8366733b330c
Vijaydwivedi10 <84119245+Vijaydwivedi10@users.noreply.github.com>
2023-07-02 11:39:42 +0530

    Update files with project content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
